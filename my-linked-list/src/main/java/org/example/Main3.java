dc3a68afc12cf8620fd1062f476ed8ec3fd20354
leejuno <zhqkfxm0127@naver.com>
2024-02-05 14:39:56 +0900

    feat(MyQueue): enqueue, printAll 기능 구현

--- my-linked-list/src/main/java/org/example/Main3.java
+++ my-linked-list/src/main/java/org/example/Main3.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Main3 {
+    public static void main(String[] args){
+        MyQueue<String> myQueue = new MyQueue<>();
+
+        myQueue.enqueue("일");
+        myQueue.enqueue("two");
+        myQueue.enqueue("삼");
+
+        myQueue.printAll();
+    }
 }
--- my-linked-list/src/main/java/org/example/MyQueue.java
+++ my-linked-list/src/main/java/org/example/MyQueue.java
@@ -1,4 +1,31 @@
 package org.example;
 
-public class MyQueue {
+public class MyQueue<T> {
+    private MyLinkedList list;
+    private Node<T> front;
+    private Node<T> back;
+    private int size;
+
+    public MyQueue(){
+        list = new MyLinkedList<T>();
+        size=0;
+    }
+
+    public void enqueue(T data){
+        list.add(data);
+        size++;
+        if(front==null && back==null){
+            front = list.getNode(size);
+            back = front;
+        }
+        back = list.getNode(size);
+    }
+
+    public void printAll(){
+        list.printAll();
+        System.out.println("size: " + size);
+        System.out.println("front: " + front.getData());
+        System.out.println("back: " + back.getData());
+    }
+
 }
