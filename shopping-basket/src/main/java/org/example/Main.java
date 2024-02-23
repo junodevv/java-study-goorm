9f265321956903dd7862c54a74fee6c238e5abc4
leejuno <zhqkfxm0127@naver.com>
2024-02-23 14:37:36 +0900

    feat(Main): 메인 클래스 기본구성

--- shopping-basket/src/main/java/org/example/Main.java
+++ shopping-basket/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 상품 목록 생성
+        Set<Product> productSet = new HashSet<>();
+        //TODO: 상품 클래스를 생성하여 상품목록에 넣는다.
+
+        // 상품 목록 확인
+        System.out.println("고유한 상품 목록:");
+        for(Product product: productSet){
+            System.out.println(product.getName() + " : " + product.getPrice());
+        }
+
+        // 장바구니 생성
+        Cart myCart = new Cart();
+
+        //TODO: 상품을 장바구니에 추가
+        //TODO: 상품을 장바구니에서 제거
+        //TODO: 장바구니에 현재 담긴 상품들을 출력 (상품이름, 각 상품의 갯수)
     }
 }
